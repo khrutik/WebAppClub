5da9e214e31c9d7de6a361efb3c96bb2f5f9ffee
Anuson Chanthasuk <khrutik@gmail.com>
2019-08-05 20:17:12 +0700

    Add files via upload

---
+++ oddeven.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>PHP TEXT BOX</title>
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<form method="post" action="">
+			Number :<br><input type="text" name="number"><br><br><input type="submit" value="Confirm">
+		</form>
+		<?php
+			$number = $_POST["number"];
+			if($number%2)
+			{
+				echo "เลขคี่";
+			}
+			else
+			{
+				echo "เลขคู่";
+			}
+		?>	
+	</body>
+</html>
