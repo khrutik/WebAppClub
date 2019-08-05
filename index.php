dd32bee40b4d527a744c52d3d2247519b020770e
Anuson Chanthasuk <khrutik@gmail.com>
2019-08-05 20:00:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>My First Page</title>
-</head>
-<body>
-<form method="post" action="">
-	Number :<br><input type="text" name="number"><br><br><input type="submit" value="Confirm">
-</form>
-
-<?php
-	echo $_POST["number"];
-?>
-
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>PHP TEXT BOX</title>
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<form method="post" action="">
+			Number :<br><input type="text" name="number"><br><br><input type="submit" value="Confirm">
+		</form>
+		<?php
+			echo $_POST["number"];
+		?>
+	</body>
 </html>
