1bf555712796dafd77b9aaa8bf110c9787145908
Anuson Chanthasuk <khrutik@gmail.com>
2019-08-05 21:21:52 +0700

    Add files via upload

---
+++ guessnumber.php
@@ -0,0 +1,41 @@
+<?php
+	$guessnumber = 101;
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>PHP TEXT BOX</title>
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<form method="post" action="">
+			Number :<br><input type="text" name="number"><br><br><input type="submit" value="ส่งข้อมูล">
+		</form>
+		<?php
+			$number = $_POST["number"];
+			
+
+			if(isset($number))
+			{
+				if($number>$guessnumber)
+				{
+					echo "มากกว่า";
+				}
+				elseif ($number<$guessnumber) {
+					echo "น้อยกว่า";
+				}
+				else
+				{
+					echo "ถูกต้องเลย";
+				}
+			}
+			elseif ($number=="") 
+			{
+				echo "กรอกข้อมูลด้วย";
+			}
+		?>
+	</body>
+</html>
